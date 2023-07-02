2231ea5de464005c75c561f504be1b8b1b0bfeee
arthur <arthursantos.oliveira001@gmail.com>
2023-07-02 15:13:45 -0300

    demais get

--- transactions/routes.js
+++ transactions/routes.js
@@ -14,4 +14,26 @@ router.get('/veiculos', (req, res) => {
     res.json(allVehicles);
 });
 
+// GET /veiculos/find?q=termo
+router.get('/veiculos/find', (req, res) => {
+    const term = req.query.q.toLowerCase();
+    const filteredVehicles = vehiclesInstance.getFilteredVehicles(term);
+    
+    res.json(filteredVehicles);
+});
+
+// GET /veiculos/:id
+router.get('/veiculos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const vehicle = vehiclesInstance.getVehicleById(id);
+    
+    if (vehicle) {
+        res.json(vehicle);
+    } else {
+        res.status(404).json({ error: 'Veículo não encontrado.' });
+    }
+});
+
+
+
 module.exports = router;
