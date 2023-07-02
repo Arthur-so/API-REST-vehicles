7403ad3aeb0a168287bd1b32d984b1298abb1125
arthur <arthursantos.oliveira001@gmail.com>
2023-07-02 17:02:26 -0300

    bubble sort

--- Exercicios/API/index.js
+++ Exercicios/API/index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const routes = require('./transactions/routes'); // Importe o arquivo de rotas
 
-// Configurações e middleware do Express
-
+// Middleware para analise JSON
 app.use(express.json());
 
 // Defina o uso das rotas
 app.use('/', routes);
 
-// Inicie o servidor
+// Inicia o servidor
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
